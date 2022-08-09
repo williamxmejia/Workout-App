a90bfd4c5019b114f61aab9dca07ea4c938786c5
williamxmejia <willx1995@gmail.com>
2022-08-09 18:39:43 -0500

    adding workout of users to profile

--- src/main/java/com/example/gymtracker/repositories/WorkoutRepository.java
+++ src/main/java/com/example/gymtracker/repositories/WorkoutRepository.java
@@ -1,9 +1,17 @@
 package com.example.gymtracker.repositories;
 
+import com.example.gymtracker.models.User;
 import com.example.gymtracker.models.Workout;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+    List<Workout> findByUser(User user);
+
+
+
+
 
 
 }
