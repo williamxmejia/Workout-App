c21c7b668862b756809241c416e2566c9481c51e
williamxmejia <willx1995@gmail.com>
2022-08-11 00:44:38 -0500

    adding a table to the home page

--- src/main/java/com/example/gymtracker/controllers/HomeController.java
+++ src/main/java/com/example/gymtracker/controllers/HomeController.java
@@ -1,13 +1,32 @@
 package com.example.gymtracker.controllers;
 
+import com.example.gymtracker.models.Workout;
+import com.example.gymtracker.repositories.WorkoutRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final WorkoutRepository workoutDao;
+
+    public HomeController(WorkoutRepository workoutDao) {
+        this.workoutDao = workoutDao;
+    }
+
     @GetMapping("/")
-    public static String home(){
+    public String home(Model model) {
+
+        List<Workout> workoutList = workoutDao.findAll();
+
+        Collections.reverse(workoutList);
+
+        model.addAttribute("workout", workoutList);
+
         return "views/home";
     }
 
